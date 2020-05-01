4c5515dc8e5bbe51925ace9fa039b31bdd959203
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-05-01 10:36:44 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Cocktail is a well-iced mixed drink made up of a base liquor, a
+        modifying ingredient as a modifier, and a special flavoring or coloring
+        agents. It is usually an apperitiff taken at leisure before a meal to
+        wet the appetite, it is also reputedly aids digestion
+      </p>
+    </section>
+  );
+}
